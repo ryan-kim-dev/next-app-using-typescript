4f52116c7442d52490f9f23d008ad42709124a4b
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-10-31 22:26:42 +0900

    pages 폴더의 라우팅 경로 연습용 컴포넌트 생성

---
+++ practice/pages/submenu/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+function About() {
+  return (
+    <>
+      <h1>/pages/submenu/about.tsx</h1>
+      <Link href="/">/pages/index.tsx</Link>
+    </>
+  );
+}
+
+export default About;
---
+++ practice/pages/submenu/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function SubIndex() {
+  return (
+    <>
+      <h1>/pages/submenu/index.tsx</h1>
+      <Link href="/">/pages/index.tsx</Link>
+    </>
+  );
+}
