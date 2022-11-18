7578b00655454099faa065f17d379e321b8078ea
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-11-18 17:38:57 +0900

    feat: 정적웹 빌드 및 배포

--- next-guide/pages/_app.tsx
+++ next-guide/pages/_app.tsx
@@ -2,10 +2,5 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      This is _app
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
