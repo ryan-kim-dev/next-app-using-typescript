eb8dbf4e16324efd7efe65cf05890d10d4a00dea
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-11-26 11:58:39 +0900

    Feat: API데이터 axios get 예제 구현

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 type Props = {};
@@ -7,24 +8,42 @@ export default function Navbar({}: Props) {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-        <span className={router.pathname === '/' ? 'active' : ''}>홈</span>
-      </Link>
-      <Link href="/about">
-        <span className={router.pathname === '/about' ? 'active' : ''}>
-          About
-        </span>
-      </Link>
+      <Image src="/vercel.svg" alt="logo" width={100} height={100} />
+      <div>
+        <Link href="/">
+          <span className={router.pathname === '/' ? 'active' : ''}>홈</span>
+        </Link>
+        <Link href="/about">
+          <span className={router.pathname === '/about' ? 'active' : ''}>
+            About
+          </span>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          border: 1px solid green;
-          color: red;
           display: flex;
-          justify-content: space-evenly;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: red;
-          border: 3px solid red;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,10 +1,46 @@
+import { useState, useEffect } from 'react';
 import Seo from '../components/Seo';
+import axios from 'axios';
+
+const API_KEY = 'c14e7f431d238127cb4acc682f92c9f8';
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // async IIFE
+    (async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      const { results } = await res.data;
+      return setMovies(results);
+    })();
+  }, []);
+
+  type MovieProps = {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  };
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>홈</h1>
+      {movies.map((movie: MovieProps) => {
+        return <div key={movie.id}></div>;
+      })}
     </div>
   );
 }
