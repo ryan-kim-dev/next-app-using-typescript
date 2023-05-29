7c323bb55d9ffe21a63e6dabd905d82bfa1c3187
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-05-29 23:27:47 +0900

    Feat: Implement presenting a state of questions length & current question idx to UI

--- quiz-app/app/quiz/page.tsx
+++ quiz-app/app/quiz/page.tsx
@@ -1,5 +1,33 @@
+'use client';
+import { useState } from 'react';
+import { quiz } from '../data';
+
 const page = () => {
-  return <div>page</div>;
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIdx, setSelectedAnswerIdx] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  // 디스트럭쳐링 할당
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+  console.log(activeQuestion);
+
+  return (
+    <div className="container">
+      <h1>퀴즈 페이지</h1>;
+      <h2>
+        문제: {activeQuestion + 1}
+        <span>/{questions.length}</span>
+      </h2>
+    </div>
+  );
 };
 
 export default page;
